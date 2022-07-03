a5c224f846fd2198a9a83fb881083df59a6a0eb0
Evgeny Was <evgeniywas@gmail.com>
2022-07-03 17:00:06 +0300

    update: add offers component and its styles

--- src/pages/StaysPage.tsx
+++ src/pages/StaysPage.tsx
@@ -1,11 +1,13 @@
 import HomePage from '../components/HomePage/HomePage';
 import HotelsBlock from '../components/Hotels/HotelsBlock';
+import Offers from '../components/Offers/Offers';
 import RefsContextProvider from '../context/RefsContextProvider';
 
 function StaysPage() {
   return (
     <RefsContextProvider>
       <HomePage />
+      <Offers />
       <HotelsBlock />
     </RefsContextProvider>
   );
